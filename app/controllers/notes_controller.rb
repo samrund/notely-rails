65fb4c6a99c5cc216c4d4babcbda6a703a007ed5
Dave Strus <dave@davestrus.com>
2015-05-07 14:20:49 -0400

    Create and update Notes.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -8,9 +8,34 @@ class NotesController < ApplicationController
   def edit
   end
 
+  def create
+    @note = Note.new note_params
+    if @note.save
+      flash.now[:notice] = t('note.flash.create.success')
+      render :edit
+    else
+      flash.now[:alert] = t('note.flash.create.failure')
+      render :new
+    end
+  end
+
+  def update
+    @note = Note.find params[:id]
+    if @note.update note_params
+      flash.now[:notice] = t('note.flash.update.success')
+    else
+      flash.now[:alert] = t('note.flash.update.failure')
+    end
+    render :edit
+  end
+
   private
 
   def find_note
     @note = Note.find params[:id]
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body_html, :body_text)
   end
 end
